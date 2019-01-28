7cf9263e9aef9fdecbe3157b311039bc809e6359
unknown <unknown@example.com>
2019-01-28 17:44:17 +0800

    使用子组件绑定事件来调用父组件的方法

--- pages/components/CardItem.js
+++ pages/components/CardItem.js
@@ -1,4 +1,6 @@
 // pages/CardItem.js
+const BACK = 0, RIGHT = 1, IMAGE = 2
+
 Component({
   /**
    * 组件的属性列表
@@ -9,15 +11,36 @@ Component({
       type: String,
       value: '',
     },
-    showback: {
-      type: Boolean,
-      value: true,
+    itemid:{
+      type:Number,
+      value:null
+    },
+    idx:{
+      type: Number,
+      value:null
+    },
+    status: {
+      type: Number,
+      value: BACK,
       observer: function(newVal, oldVal) {
-        console.log('observer showback : ', newVal)
-        if( true == newVal ){
-          this.setData({
-            _showback: true,
-          })
+        console.log('observer status : ', newVal)
+        // if( true == newVal ){
+        //   this.setData({
+        //     _showback: true,
+        //   })
+        // }
+        switch(newVal){
+          case BACK:
+          console.log('back')
+          break;
+          case RIGHT:
+            console.log('right')
+          break;
+          case IMAGE:
+            console.log('image')
+          break;
+          default:
+          throw new Error('status has error value:', newVal)
         }
 
       }
@@ -33,7 +56,7 @@ Component({
    * 组件的初始数据
    */
   data: {
-    _showback: false
+    _status: BACK
   },
 
   /**
@@ -42,6 +65,8 @@ Component({
   methods: {
 
     clickCard: function() {
+      const eventDetail={status: this.properties.status, idx: this.properties.idx, itemid: this.properties.itemid}
+      this.triggerEvent('myclick', eventDetail )
       this.setData({
         showEffect: 'animated rotateIn'
       })
--- pages/index/index.js
+++ pages/index/index.js
@@ -18,19 +18,21 @@ const randArray = function(src) {
   shuffle(resultArr)
   return resultArr
 }
+const BACK = 0,
+  RIGHT = 1,
+  IMAGE = 2
 
 
 var questions = [{
     src: "https://www.gsenglish.cn/pictures/20171222145725sheep31513925751.jpg",
     id: 0,
-    showback: true,
-    right: false
+    status: BACK,
+
   },
   {
     src: "https://www.gsenglish.cn/pictures/20130808071726look%20down%5Ecat%20look%20down%5Ecat%5Elooked%20down.jpg",
     id: 1,
-    showback: true,
-    right: false,
+    status: BACK
   }
 ]
 //用户的回答，也就是点击了哪个索引！
@@ -60,6 +62,15 @@ Page({
   isAnswerRight: function() {
     return g_answers[0].id == g_answers[1].id
   },
+  testChildClick: function(e) {
+    console.log('from child:', e.detail)
+    let id = e.detail.itemid
+    let index = e.detail.idx
+    g_answers.push({
+      "id": id,
+      "index": index
+    })
+  },
 
   parentClick: function(event) {
     let id = event.currentTarget.dataset.id
@@ -74,11 +85,11 @@ Page({
     if (g_answers.length == questions.length) {
       if (this.isAnswerRight()) {
         // 都不能再点击了，不然会出现其它错误！
-        
+
         //让正确的隐藏起来！比较方便笨的办法就是重新生成要显示的数组！
         let newShowArray = Array.from(this.data.showArray)
         for (var item of g_answers) {
-          newShowArray[item.index].right = true
+          newShowArray[item.index].status = RIGHT
         }
         console.log("newShowArray changed: ", g_answers, newShowArray)
 
@@ -89,7 +100,7 @@ Page({
             canClick: true,
             showArray: newShowArray
           })
-        }, 750+1000)
+        }, 750 + 1000)
 
 
         //重置回答数组
@@ -106,11 +117,11 @@ Page({
 
 
       } else {
-        
+
         console.log('wrong, 重新选择，并翻转图片的背景showback', g_answers)
         let newShowArray = Array.from(this.data.showArray)
         for (var item of g_answers) {
-          newShowArray[item.index].showback = true
+          newShowArray[item.index].status = BACK
         }
         console.log("newShowArray changed: ", g_answers, newShowArray)
 
